fa406d9f1b265decc1edb4123d859f4d38ccb8b1
Jigers Alina <alina.jigers@gmail.com>
2020-05-17 21:39:01 +0800

    'challenge-Loop-Tringle'

---
+++ Loop-Triangle.js
@@ -0,0 +1,24 @@
+/*  Write a loop that makes saven calls to 
+console.log to output the following triangle
+
+ *
+ **
+ ***
+ ****
+ *****
+ ****** */
+
+
+
+
+for (i =1; i < 7; i++) {
+    console.log('*'.repeat(i))
+}
+
+
+// reverse Tringle
+
+for (i =7; i >= 0; i-- ) {
+   console.log('*'.repeat(i)) 
+}
+
